da0ccd69239afdf7fd11590f24f25068aca42cc7
Carl Jorgensen <carl@cjorgensen.eu>
2021-01-22 15:11:03 +0100

    ajout du script list_options

---
+++ list_options.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+
+# Ce script liste les options du script sys_admin.sh
+
+echo "Options possibles :
+- installer_openstack
+- installer_git
+- installer_jenkins
+- installer_paquet
+- maj_systeme
+- afficher_paquets
+- supprimer_paquet
+- ajouter_utilisateur
+- ajouter_groupe
+- afficher_groupes_utilisateur
+- statut_service
+- redemare_service
+- active_service
+- active_service
+- help"
+
+
+
+
